25ee6001d22a28c4873fdc32dcfe9d93144fe8eb
Jakub <Wilk>
2023-02-08 09:40:10 -0600

    Initial Commit

---
+++ map hackwich/map_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_hackwichApp.swift
+//  map hackwich
+//
+//  Created by Jakub Wilk on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct map_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
